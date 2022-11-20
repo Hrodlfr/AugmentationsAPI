29a5fd11f25105546eee838fcea4dcfc8a9b71b8
TheUmbralPyre <theumbralpyre@gmail.com>
2022-11-20 16:41:07 +0200

    Add project files.

---
+++ AugmentationsAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AugmentationsAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
